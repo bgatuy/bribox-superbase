92d2fcc1388c2d3b2472e3a7b76be0ac38cc2adc
bgatuy <takiyafathur11@gmail.com>
2025-11-02 21:28:25 +0700

    first commit

--- login.js
+++ login.js
@@ -1,8 +1,8 @@
 // login.js
 
 // Ganti dengan URL dan anon key dari proyek Supabase Anda
-const SUPABASE_URL = 'https://YOR_PROJECT_ID.supabase.co';
-const SUPABASE_ANON_KEY = 'YOUR_ANON_KEY';
+const SUPABASE_URL = 'https://hfwklepqsiwubjxatmsq.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhmd2tsZXBxc2l3dWJqeGF0bXNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIwODk5MDAsImV4cCI6MjA3NzY2NTkwMH0.gWi8ukCILo11q-3wToMZQ4aHOLrp19Ss8jZoDHMJusY';
 
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
